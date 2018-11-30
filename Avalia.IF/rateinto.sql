7ff9f32c3f7a23fb91607c53ba0226fd37c013e9
Tares Orlande <taresorlande@yahoo.com.br>
2018-11-30 08:45:34 -0200

    Last Step
    This reverts commit b8e0e913b4b9dde4b6d558fe1009fb77ec0809ae.

---
+++ Avalia.IF/rateinto.sql
@@ -0,0 +1,37 @@
+use ´rate´;
+
+insert into `star` (`id_post`,`ip`,`rate`) values (1,'192.168.15.13',4);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (2,'192.168.15.13',3);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (3,'192.168.15.13',5);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (4,'192.168.15.13',2);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (3,'192.168.15.13',5);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (1,'192.168.15.13',1);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (2,'192.168.15.13',4);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (4,'192.168.15.13',2);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (3,'192.168.15.13',5);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (1,'192.168.15.13',1);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (2,'192.168.15.13',1);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (2,'192.168.15.13',3);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (3,'192.168.15.13',5);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (4,'192.168.15.13',2);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (2,'192.168.15.13',1);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (3,'192.168.15.13',5);
+
+insert into `star` (`id_post`,`ip`,`rate`) values (4,'192.168.15.13',1);
+
+
